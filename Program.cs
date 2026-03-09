0278e5b31265149006b6c9ad2d06a891904b27c6
Muhammad Irfan <irfanmrfn28@gmail.com>
2026-03-09 07:18:28 +0700

    Lesson 2: Implemented database GET endpoints and resolved JSON object cycle exception

--- Program.cs
+++ Program.cs
@@ -1,18 +1,51 @@
+using AspNetCoreApi.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-// Endpoint 1: The Root (A simple health check)
-app.MapGet("/", () => "Welcome to the C# Bank API! The server is running perfectly.");
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+                          ?? throw new InvalidOperationException("Connection string not found.");
 
-// Endpoint 2: A Custom JSON Response
-app.MapGet("/api/status", () =>
+builder.Services.AddDbContext<BankContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options =>
 {
-    return new
-    {
-        Message = "All systems operational",
-        ServerTime = DateTime.UtcNow,
-        Version = "1.0.0"
-    };
+   options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles; 
 });
 
-app.Run();
+var app = builder.Build();
+
+// Endpoint 1: Get all accounts
+app.MapGet("/api/accounts", async (BankContext db) =>
+{
+    var accounts = await db.Accounts.ToListAsync();
+
+    if (accounts.Count == 0)
+    {
+        // Results.NotFound() creates a standard 404 HTTP response.
+        return Results.NotFound(new { Message = "No accounts found in the database."});
+    }
+
+    // Results.Ok() creates a standard 200 HTTP response.
+    return Results.Ok(accounts);
+});
+
+// Endpoint 2: Get a specific account by ID (e.g., /api/account/1)
+app.MapGet("/api/accounts/{id}", async (int id, BankContext db) =>
+{
+   // Fetch the account and its transactions using Eager Loading
+   var account = await db.Accounts
+        .Include(a => a.Transactions)
+        .FirstOrDefaultAsync(a => a.Id == id); 
+
+    if (account == null)
+    {
+        return Results.NotFound(new { Message = $"Account with ID {id} not found." });
+    }
+
+    return Results.Ok(account);
+});
+
+await app.RunAsync();
